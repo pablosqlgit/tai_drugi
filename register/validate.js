81358544c852529a26f90aabea455e5a84e77fc8
Kacper Pawlak <126149039+pablosqlgit@users.noreply.github.com>
2023-11-05 13:46:18 +0100

    register frontend done

--- register/validate.js
+++ register/validate.js
@@ -7,50 +7,71 @@ const control = document.querySelector(".pass-control")
 const codeSelect = document.querySelector(".code-select")
 const codesList = document.querySelector(".codes-list")
 const selectedCode = document.querySelector(".selected-code")
+const rPassControl = document.querySelector(".rPass-control")
+const loginControl = document.querySelector(".login-control")
+const passControl = document.querySelector(".pass-control")
+const phoneControl = document.querySelector(".phone-control")
+const codeSearch = document.querySelector(".code-search")
 
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const capital = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-const special = ["!", "#", "$", "&"]
-const notAllowed = ["@", "$", "%", "^", "*", "(", ")", "<", ">", "?", "/", ",", ".", "[", "]", "{", "}", "'", '"', "`", "~", "|"]
+const special = ["!", "#", "$", "&", "@"]
+const notAllowed = ["%", "^", "*", "(", ")", "<", ">", "?", "/", ",", ".", "[", "]", "{", "}", "'", '"', "`", "~", "|"]
 
 let codes = []
 
 let select = "+48"
+let ok = 0
 selectedCode.innerText = select
 
-fetch("./codes.json")
+const notSame = (item) => {
+  if(item.target.name === 'pass'){
+    if(item.target.value !== rPass.value) rPassControl.className = "different" 
+    else rPassControl.className = "rPass-control"
+  }
+  if(item.target.name === 'rPass'){
+    if(item.target.value !== pass.value) rPassControl.className = "different" 
+    else rPassControl.className = "rPass-control"
+  }
+}
+
+fetch("./codes2.json")
 .then(res => res.json())
 .then(res => {
-  codes = res.codes
-  codes.forEach(code => {
-    codesList.innerHTML += `<p class="code">${code.dial_code}</p>`
-  })
+  for(let [key, value] of Object.entries(res)){
+    const dialCode = value.phone[0].replace('-', '')
+    codesList.innerHTML += `<div class='dial-code-container'><img src=${value.image} class='flag-icon'><p class="code">${dialCode}</p></div>`
+  }
 })
-.catch(err => alert("codes error"))
+.catch(err => alert("codes error" + err))
 
 document.body.addEventListener("click", (e) => {
+  if(e.target.className === 'dial-code-search'){
+    codesList.style.display = 'block'
+  }
   if(e.target.className === "code-select" || e.target.className === "selected-code"){
     codesList.style.display === "none" || codesList.style.display === ""
     ? codesList.style.display = "block"
     : codesList.style.display = "none"
-  }else if(codesList.style.display === "block" && e.target.className !== "code") codesList.style.display = "none"
+  }
+  if(e.target.className === ''){
+    if(codesList.style.display === "block"){
+      codesList.style.display = 'none'
+    }
+  }
 })
 
 codesList.addEventListener("click", (e) => {
-  select = e.target.innerText
-  selectedCode.innerText = select
-  codesList.style.display = "none"
+  if(e.target.className === 'code'){
+    select = e.target.innerText
+    selectedCode.innerText = select
+    codesList.style.display = "none"
+  }
 })
 
-console.log('ds')
-
-submit.addEventListener("click", e => {
-  e.preventDefault()
-});
-
 pass.addEventListener("input", e => {
-  console.log(e.target.value.length)
+  notSame(e)
   if(e.target.value.length < 6){
     control.className = "length"
   }else{
@@ -69,14 +90,41 @@ pass.addEventListener("input", e => {
     if(specialCount === 0 || numbersCount === 0 || capitalCount === 0 || lettersCount < 3){
       control.className = "symbols"
     }else if(specialCount >= 1 && numbersCount >= 1){
-      control.className = ""
+      control.className = "pass-control"
     }else if(e.target.value.length > 6){
       control.className = "symbols"
     }
-    console.log(notAllowedChar)
     if(notAllowedChar > 0){
-      console.log("są")
       control.className = "not-allowed"
     }
   }
 })
+
+rPass.addEventListener("input", e => {
+  notSame(e)
+})
+
+login.addEventListener("input", e => {
+  for(let i in e.target.value){
+    if(notAllowed.includes(e.target.value[i])){
+      loginControl.className = 'not-allowed'
+      break
+    }else{
+      loginControl.className = 'login-control'
+    }
+  }
+  if(e.target.value.length < 6){
+    loginControl.className = 'login-length'
+  }
+})
+
+submit.addEventListener("click", e => {
+  if(
+    rPassControl.className !== 'rPass-control'
+    && loginControl.className !== 'login-control'
+    && passControl.className !== 'pass-control'
+    && phoneControl.className !== 'phone-control'
+  ){
+    e.preventDefault()
+  }
+});
