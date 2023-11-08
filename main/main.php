ec8542a6d7fe1391616e5b5164f07302b177d8ba
Kacper Pawlak <126149039+pablosqlgit@users.noreply.github.com>
2023-11-08 15:26:41 +0100

    done home page

--- main/main.php
+++ main/main.php
@@ -8,8 +8,64 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Strona Główna</title>
+  <link rel="stylesheet" href="style.css" />
 </head>
 <body>
-  <h1>Strona Główna</h1>
+  <nav>
+    <span>
+      <h1>
+        Witaj, {username}!
+      </h1>
+    </span>
+    <div class='functional-buttons'>
+      <form>
+        <input type='submit' value='Moje konto' />      
+      </form>
+      <form>
+        <input type='submit' value='Wyloguj się' />      
+      </form>
+    </div>
+  </nav>
+  <article>
+    <section>
+      <?php 
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/tai_drugi/conn.php';
+
+        $selectQ = "SELECT * FROM events INNER JOIN images ON images.eventID = events.id INNER JOIN cheapest ON cheapest.eventID = events.id;";
+        $res = mysqli_query($conn, $selectQ);
+
+        $ticketsQ = "SELECT * FROM events INNER JOIN images ON images.eventID = events.id";
+      
+        if(mysqli_num_rows($res) > 0){
+          while($row = mysqli_fetch_assoc($res)){
+            echo "
+              <div class='concert-container'>
+                <div class='event-image'>
+                  <img src='../images/$row[src]'>
+                </div>
+                <div class='event-data'>
+                  <span class='event-name'>
+                    $row[name]
+                  </span>
+                  <span class='event-location'>
+                    Lokalizacja: $row[location]
+                  </span>
+                  <span class='event-date'>
+                    Data: $row[date]
+                  </span>
+                  <span class='tickets-price'>
+                    Bilety od: $row[lowest_value]PLN
+                  </span>
+                </div>
+                <div class='buy-ticket'>
+                  <a href='event.php?event=$row[id]'>Kup bilet</a>
+                </div>
+              </div>
+            ";
+          }
+        }
+      ?>
+    </section>
+  </article>
 </body>
 </html>
